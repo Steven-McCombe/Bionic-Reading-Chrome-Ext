89315311b9713f9f9dc6431df4bee873891c7287
steven mccombe <smccombe93@gmail.com>
2023-05-12 10:19:42 -0400

    Bug fixing

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
 //This script listens for when the user clicks the extension's icon in the Chrome toolbar. 
 
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ['content.js']
-    });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete') {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ['content.js']
+        });
+    }
 });
